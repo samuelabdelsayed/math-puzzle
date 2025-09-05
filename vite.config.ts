6beba8adcb943e126dfface1ab094c429b3905ec
Samuel Abdelsayed <samuel.abdelsayed@example.com>
2025-09-06 00:29:42 +1000

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/math-puzzle/', // Base path for GitHub Pages
   server: {
     open: true
   }
